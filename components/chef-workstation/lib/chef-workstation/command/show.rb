05acabae2367e0bef622b20c4a8ce26e7af2611b
tyler-ball <tyleraball@gmail.com>
2018-03-02 12:22:06 -0700

    Fixing style issues

--- components/chef-workstation/lib/chef-workstation/command/show.rb
+++ components/chef-workstation/lib/chef-workstation/command/show.rb
@@ -1,6 +1,6 @@
 # TODO eventually when we have more commands we will want to do something closer to Shake Shack where we have a commands map and all the commands inherit from a base class. But thats too much scope creep for the current PR
-require 'pp'
-require 'chef-workstation/config'
+require "pp"
+require "chef-workstation/config"
 
 module ChefWorkstation
   module Command
