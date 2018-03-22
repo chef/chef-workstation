2c9478a78b6b18f5e28a96cce811bd55e966e791
Jaymala Sinha <jsinha@chef.io>
2018-03-22 17:09:38 -0400

    We should not update the Gemfile version dependency
    Noticed build failure with this
    
    Signed-off-by: Jaymala Sinha <jsinha@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -7,9 +7,3 @@
 set -evx
 
 sed -i -r "s/VERSION = '.*'/VERSION = '$(cat VERSION)'/"  components/chef-workstation/lib/chef-workstation/version.rb
-
-# Update the version inside Gemfile.lock
-cd components/chef-workstation; bundle update chef-workstation
-
-# Once Expeditor finshes executing this script, it will commit the changes and push
-# the commit as a new tag corresponding to the value in the VERSION file.
