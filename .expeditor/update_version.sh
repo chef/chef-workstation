eb8ed45894a10a48f593041a8f40ff247d8ed735
Jon Morrow <jmorrow@chef.io>
2018-04-04 17:40:35 -0700

    Try using bash instead of sh in expeditor.
    Signed-off-by: Jon Morrow <jmorrow@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 #
 # After a PR merge, Chef Expeditor will bump the PATCH version in the VERSION file.
 # It then executes this file to update any other files/components with that new version.
@@ -16,4 +16,4 @@ cd ../..
 # run readme update script.
 # TODO: Remove this when expeditor issue requiring mixlib install definition
 # is fixed. This check prevents us changing our config file.
-.expeditor/update_readme_download_urls.sh
+bash .expeditor/update_readme_download_urls.sh
