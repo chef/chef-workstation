1972922e05d66a13a8d55d58730e9fec9c8a9088
tyler-ball <tyleraball@gmail.com>
2018-04-26 14:39:32 -0600

    Removing thread ability to print exception information to stdout

--- components/chef-workstation/lib/chef-workstation/ui/terminal.rb
+++ components/chef-workstation/lib/chef-workstation/ui/terminal.rb
@@ -13,6 +13,11 @@ module ChefWorkstation
 
         def init(location)
           @location = location
+          # In Ruby 2.5+ threads print out to stdout when they raise an exception. This is an agressive
+          # attempt to ensure debugging information is not lost, but in our case it is not necessary
+          # because we handle all the errors ourself. So we disable this to keep output clean.
+          # See https://ruby-doc.org/core-2.5.0/Thread.html#method-c-report_on_exception
+          Thread.report_on_exception = false
         end
 
         def write(msg)
