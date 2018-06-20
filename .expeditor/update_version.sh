59b7c501f56dd702d1752cd0b62a6c84e6fedca3
tyler-ball <tball@chef.io>
2018-06-20 14:53:11 -0600

    We no longer need to install bundler because lita workers have ChefDK installed
    Signed-off-by: tyler-ball <tball@chef.io>

--- .expeditor/update_version.sh
+++ .expeditor/update_version.sh
@@ -9,10 +9,6 @@ set -evx
 sed -i -r "s/VERSION = \".*\"/VERSION = \"$(cat VERSION)\"/"  components/chef-cli/lib/chef-cli/version.rb
 sed -i -r "s/VERSION = \".*\"/VERSION = \"$(cat VERSION)\"/"  components/chef-run/lib/chef-run/version.rb
 
-# Temporary workound while ChefDK has older ruby version
-export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
-gem install bundler
-
 # Ensure our Gemfile.lock reflects the new version
 cd components/chef-cli
 bundle update chef-cli
