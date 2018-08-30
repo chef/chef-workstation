d3de34e33f2881a01398d3e0c9b6994b4f7cf28d
Marc A. Paradise <marc.paradise@gmail.com>
2018-08-30 14:16:02 -0400

    Manually pack linux tray archive to prevent failures on rhel 6
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- omnibus/config/software/chef-workstation-tray.rb
+++ omnibus/config/software/chef-workstation-tray.rb
@@ -15,26 +15,39 @@ build do
     app_version = JSON.parse(File.read(File.join(project_dir, "package.json")))["version"]
 
     env = with_standard_compiler_flags(with_embedded_path)
-    platform_name, artifact_path, artifact_ext = if mac_os_x?
-                                                   ["mac", "Chef Workstation App-#{app_version}-mac", "7z"]
-                                                 elsif linux?
-                                                   ["linux", "chef-workstation-app-#{app_version}", "tar.gz"]
-                                                 elsif windows?
-                                                   ["win", "Chef Workstation App-#{app_version}-win", "7z"]
-                                                 end
+    platform_name, artifact_path = if mac_os_x?
+                                     ["mac", "Chef Workstation App-#{app_version}-mac"]
+                                   elsif linux?
+                                     # For linux we're using directories - electron's packageer
+                                     # fails on RHEL because of bad GLIBC version.
+                                     # Instead we'll manually create an archive for linux platforms.
+                                     #
+                                     ["linux", "chef-workstation-app-#{app_version}"]
+                                   elsif windows?
+                                     ["win", "Chef Workstation App-#{app_version}-win"]
+                                   end
     separator = File::PATH_SEPARATOR || ":"
     node_bin_path = File.join(install_dir, "embedded", "nodejs", "bin")
+    installer_dir = "#{install_dir}/installers"
+    dist_dir = File.join(project_dir, "dist")
+
     env["PATH"] = "#{env["PATH"]}#{separator}#{node_bin_path}"
     # Windows
     env["Path"] = "#{env["Path"]}#{separator}#{node_bin_path}"
 
+    FileUtils.rm_rf(dist_dir)
     command "npm install", env: env
     command "npm run-script build-#{platform_name}", env: env
 
-    target = "#{install_dir}/installers/chef-workstation-app-#{platform_name}.#{artifact_ext}"
-    mkdir File.dirname(target)
+    mkdir installer_dir
 
-    copy "#{project_dir}/dist/#{artifact_path}", "#{target}"
+    if linux?
+      target = File.join(installer_dir, "chef-workstation-app-#{platform_name}.tar.gz")
+      command "tar -f #{target} -C dist/linux-unpacked -cz .", env: env
+    else
+      target = File.join(installer_dir, "chef-workstation-app-#{platform_name}.zip")
+      copy File.join(dist_dir, artifact_path), target
+    end
   end
 end
 # TODO cleanup step that removes node binaries so they're not packaged.
