e377b49750e7e109d5343d2df9554b577f7791bd
Marc A. Paradise <marc.paradise@gmail.com>
2018-08-30 00:48:06 -0400

    platform build fixes
    - look for .tar.gz instead of 7z on linux - workaround
      for electron packaging failure for GLIBC2.4 missing on RHEL6
    - attempt setting `Path` in addition to `PATH` for Windows -
      build output shows that the value we added for `PATH` was
      not present.
    
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

--- omnibus/config/software/chef-workstation-tray.rb
+++ omnibus/config/software/chef-workstation-tray.rb
@@ -15,21 +15,23 @@ build do
     app_version = JSON.parse(File.read(File.join(project_dir, "package.json")))["version"]
 
     env = with_standard_compiler_flags(with_embedded_path)
-    platform_name, artifact_path = if mac_os_x?
-                                     ["mac", "Chef Workstation App-#{app_version}-mac.7z"]
-                                   elsif linux?
-                                     ["linux", "chef-workstation-app-#{app_version}.7z"]
-                                   elsif windows?
-                                     ["win", "Chef Workstation App-#{app_version}-win.7z"]
-                                   end
+    platform_name, artifact_path, artifact_ext = if mac_os_x?
+                                                   ["mac", "Chef Workstation App-#{app_version}-mac", "7z"]
+                                                 elsif linux?
+                                                   ["linux", "chef-workstation-app-#{app_version}", "tar.gz"]
+                                                 elsif windows?
+                                                   ["win", "Chef Workstation App-#{app_version}-win", "7z"]
+                                                 end
     separator = File::PATH_SEPARATOR || ":"
     node_bin_path = File.join(install_dir, "embedded", "nodejs", "bin")
     env["PATH"] = "#{env["PATH"]}#{separator}#{node_bin_path}"
+    # Windows
+    env["Path"] = "#{env["Path"]}#{separator}#{node_bin_path}"
 
     command "npm install", env: env
     command "npm run-script build-#{platform_name}", env: env
 
-    target = "#{install_dir}/installers/chef-workstation-app-#{platform_name}.7z"
+    target = "#{install_dir}/installers/chef-workstation-app-#{platform_name}.#{artifact_ext}"
     mkdir File.dirname(target)
 
     copy "#{project_dir}/dist/#{artifact_path}", "#{target}"
