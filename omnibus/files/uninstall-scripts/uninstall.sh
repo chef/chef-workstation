df3a2dd096506ece5def0a3e7654ca3ae74f1b75
Jon Morrow <jmorrow@chef.io>
2018-10-11 22:24:52 -0700

    Clean up user messaging order in uninstall script
    1. Prompt user for password before closing app message during uninstall.
    2. Only show closing app message if app is running.
    
    Signed-off-by: Jon Morrow <jmorrow@chef.io>

--- omnibus/files/uninstall-scripts/uninstall.sh
+++ omnibus/files/uninstall-scripts/uninstall.sh
@@ -18,21 +18,20 @@ is_darwin()
 }
 
 if is_darwin; then
-  echo "This uninstaller will remove Chef Workstation"
-  if ! sudo -n true 2>/dev/null; then 
-    echo "Enter your password to continue"
-  fi
-  echo "Closing Chef Workstation App"
-  osascript -e 'quit app "Chef Workstation App"'
+  echo "This uninstaller will remove Chef Workstation."
   sudo -s -- <<EOF
-echo "Uninstalling Chef Workstation"
-echo "Removing files"
+if [ $(osascript -e 'application "Chef Workstation App" is running') = 'true' ]; then
+  echo "Closing Chef Workstation App..."
+  osascript -e 'quit app "Chef Workstation App"' > /dev/null 2>&1;
+fi
+echo "Uninstalling Chef Workstation..."
+echo "  -> Removing files..."
 sudo rm -rf '/opt/chef-workstation'
 sudo rm -rf '/Applications/Chef Workstation App.app'
-echo "Removing binary links in /usr/local/bin"
+echo "  -> Removing binary links in /usr/local/bin..."
 sudo find /usr/local/bin -lname '/opt/chef-workstation/*' -delete
-echo "Forgeting com.getchef.pkg.chef-workstation package"
-sudo pkgutil --forget com.getchef.pkg.chef-workstation
-echo "Chef Workstation Uninstalled"
+echo "  -> Forgeting com.getchef.pkg.chef-workstation package..."
+sudo pkgutil --forget com.getchef.pkg.chef-workstation > /dev/null 2>&1;
+echo "Chef Workstation Uninstalled."
 EOF
 fi
