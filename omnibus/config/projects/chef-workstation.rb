660f377d659602c6ab86929404db982e929ae780
Jon Morrow <jmorrow@chef.io>
2018-10-17 14:19:26 -0700

    Bump DK version to v3.4.21
    Signed-off-by: Jon Morrow <jmorrow@chef.io>

--- omnibus/config/projects/chef-workstation.rb
+++ omnibus/config/projects/chef-workstation.rb
@@ -52,7 +52,7 @@ build_iteration 1
 # then Expeditor takes that version, runs a script to replace it here and pushes
 # a new commit / build through.
 
-override :"chef-dk", version: "v3.4.18"
+override :"chef-dk", version: "v3.4.21"
 
 # The Chef Workstation App version is pinned by Expeditor. Whenever Chef Workstation
 # App is merged then Expeditor takes the latest tag, runs a script to replace it here
