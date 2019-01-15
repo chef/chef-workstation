894a1a31821deef18990a637626f4cb94a495134
Jon Morrow <jmorrow@chef.io>
2019-01-15 15:02:42 -0800

    Fixes from code review
    - Moves overrides to omnibus_overrides.rb to avoid dirtying the cache.
    - Fixes sed commands.
    
    Signed-off-by: Jon Morrow <jmorrow@chef.io>

--- .expeditor/update_delivery-cli_to_latest.sh
+++ .expeditor/update_delivery-cli_to_latest.sh
@@ -5,23 +5,24 @@
 #
 # Chef Workstation uses Expeditor to manage the bundled version of the Delivery CLI.
 # Currently we always want to include the latest version of Delivery CLI inside
-# Workstation, so this script takes that version and uses sed to insert
-# it into the omnibus project definition. Then it commits that change
-# and opens a pull request for review and merge.
+# Workstation, so this script takes that version and uses sed to insert it into the
+# omnibus_overrides.rb file. Then it commits that change and opens a pull request for
+# review and merge.
 ############################################################################
 
 set -evx
 
-branch="expeditor/delivery_cli_${VERSION}"
+version=$(get_github_file $REPO master VERSION)
+branch="expeditor/delivery-cli_${version}"
 git checkout -b "$branch"
 
-sed -i -r "s/override :\"delivery-cli\",\s+version: \"v[^\"]+\"/override :\"delivery-cli\", version: \"v${VERSION}\"/" omnibus/config/projects/chef-workstation.rb
+sed -i -r "s/override \"delivery-cli\",\s+version: \"[^\"]+\"/override \"delivery-cli\", version: \"${version}\"/" omnibus_overrides.rb
 
 git add .
 
 # give a friendly message for the commit and make sure it's noted for any future audit of our codebase that no
 # DCO sign-off is needed for this sort of PR since it contains no intellectual property
-git commit --message "Bump Delivery CLI to $VERSION" --message "This pull request was triggered automatically via Expeditor when Delivery CLI $VERSION was merged to master." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
+git commit --message "Bump Delivery CLI to $version" --message "This pull request was triggered automatically via Expeditor when Delivery CLI $version was merged to master." --message "This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required."
 
 open_pull_request
 
