a9dc1ccb38dad9a1760f6da75c59578170291fe5
tyler-ball <tball@chef.io>
2019-05-31 01:42:54 -0600

    Fixing announce_release script to use appropriate expeditor variables
    Signed-off-by: tyler-ball <tball@chef.io>

--- .expeditor/announce-release.sh
+++ .expeditor/announce-release.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-set -eou pipefail
+set -exou pipefail
 
 # Download the release-notes for our specific build
 curl -o release-notes.md "https://packages.chef.io/release-notes/${EXPEDITOR_PRODUCT_KEY}/${EXPEDITOR_VERSION}.md"
 
 topic_title="Chef Workstation $EXPEDITOR_VERSION Released!"
 topic_body=$(cat <<EOH
-We are delighted to announce the availability of version $VERSION of Chef Workstation.
+We are delighted to announce the availability of version $EXPEDITOR_VERSION of Chef Workstation.
 $(cat release-notes.md)
 ---
 ## Get the Build
 
-If you are running the experimental application you can download this version from the menu after the app next update check. You can also download binaries directly from [downloads.chef.io](https://downloads.chef.io/$PRODUCT_KEY/$VERSION).
+If you are running the experimental application you can download this version from the menu after the app next update check. You can also download binaries directly from [downloads.chef.io](https://downloads.chef.io/$EXPEDITOR_PRODUCT_KEY/$EXPEDITOR_VERSION).
 
 As always, we welcome your feedback and invite you to contact us directly or share your [email](mailto:workstation@chef.io). Thanks for using Chef Workstation!
 EOH
