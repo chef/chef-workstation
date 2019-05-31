8bd67a35d8ddeb1dec12282df3fa25c1fb62fd98
Chef Expeditor <chef-ci@chef.io>
2019-05-31 04:03:15 +0000

    Bump ChefDK to 3.11.3
    This pull request was triggered automatically via Expeditor when ChefDK 3.11.3 was promoted to stable.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,7 +3,7 @@
 # Whenever ChefDK is promoted to stable or workstation app and delivery cli are merged
 # to master then Expeditor takes that version, runs a script to replace it here and pushes
 # a new commit / build through.
-override :"chef-dk", version: "v3.10.1"
+override :"chef-dk", version: "v3.11.3"
 override "delivery-cli", version: "0.0.52"
 override "chef-workstation-app", version: "v0.1.7"
 # /DO NOT MODIFY
