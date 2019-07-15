e5b730c9307c4e9ca986cd9ea3401c2062c2121c
Tim Smith <tsmith84@gmail.com>
2019-07-15 10:50:11 -0700

    Pin to rubygems 3.0.3 and update wording in omnibus_overrides
    We need 3.0.3 to prevent double bundler
    
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -8,11 +8,11 @@ override "delivery-cli", version: "0.0.52"
 override "chef-workstation-app", version: "v0.1.8"
 # /DO NOT MODIFY
 
-# DK's overrides; god have mercy on my soul
-# This comes from DK's ./omnibus_overrides.rb
-# If this stays, may need to duplicate that file and the rake
-# tasks for updating dependencies
-override :rubygems, version: "3.0.4"
+# THIS PART IS HAND MANAGED, JUST EDIT THE THING
+#
+# NOTE: You MUST update omnibus-software when adding new versions of
+# software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
+override :rubygems, version: "3.0.3" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which results in performance issues / CLI warnings. Make sure these versions match before bumping either.
 override :bundler, version: "1.17.2" # currently pinned to what ships in Ruby to prevent double bundler
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
