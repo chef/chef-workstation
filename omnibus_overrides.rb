43ca5d4157d1863be4f931c7b5f6b6a57f037101
Salim Afiune <afiune@chef.io>
2019-07-15 22:37:34 +0200

    Revert "Merge pull request #409 from chef/cleanup"
    This reverts commit 370f45d9ceac24e08d8952ea8eeb51c3f42369d3, reversing
    changes made to a93e7f70aabb1c35ac7f4b5e6d430b61ae7786cc.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -1,5 +1,5 @@
-# DO NOT MODIFY THIS SECTION
-# The delivery-cli, and workstation app versions are pinned by Expeditor.
+# DO NOT MODIFY
+# The ChefDK, delivery-cli, and workstation app versions are pinned by Expeditor.
 # Whenever ChefDK is promoted to stable or workstation app and delivery cli are merged
 # to master then Expeditor takes that version, runs a script to replace it here and pushes
 # a new commit / build through.
@@ -8,11 +8,11 @@ override "delivery-cli", version: "0.0.52"
 override "chef-workstation-app", version: "v0.1.8"
 # /DO NOT MODIFY
 
-# THIS PART IS HAND MANAGED, JUST EDIT THE THING
-#
-# NOTE: You MUST update omnibus-software when adding new versions of
-# software here: bundle exec rake dependencies:update_omnibus_gemfile_lock
-override :rubygems, version: "3.0.3" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which results in performance issues / CLI warnings. Make sure these versions match before bumping either.
+# DK's overrides; god have mercy on my soul
+# This comes from DK's ./omnibus_overrides.rb
+# If this stays, may need to duplicate that file and the rake
+# tasks for updating dependencies
+override :rubygems, version: "3.0.4"
 override :bundler, version: "1.17.2" # currently pinned to what ships in Ruby to prevent double bundler
 override "libffi", version: "3.2.1"
 override "libiconv", version: "1.15"
