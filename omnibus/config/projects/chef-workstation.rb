eb4071733ebf1b9e4b43aa1931101046fcf618bf
Seth Chisamore <schisamo@chef.io>
2019-08-13 11:15:45 -0400

    Begin signing MSI's with renewed Windows Signing Cert
    Signed-off-by: Seth Chisamore <schisamo@chef.io>

--- omnibus/config/projects/chef-workstation.rb
+++ omnibus/config/projects/chef-workstation.rb
@@ -123,7 +123,7 @@ package :msi do
   fast_msi true
   upgrade_code "9870C512-DF2C-43D9-8C28-7ACD60ABBE27"
   wix_light_extension "WixUtilExtension"
-  signing_identity "E05FF095D07F233B78EB322132BFF0F035E11B5B", machine_store: true
+  signing_identity "AF21BA8C9E50AE20DA9907B6E2D4B0CC3306CA03", machine_store: true
 end
 
 # We don't support appx builds, and they eat a lot of time.
