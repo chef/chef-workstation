ea44c123af5d9dd80092d09541e20d4f1c8ffe04
Salim Afiune <afiune@chef.io>
2019-09-23 16:44:40 -0600

    Adding few usable example of new top-level binary
    At the very beginning we want to pass every sub-command to the old 'chef'
    CLI binary that was renamed to 'chef-cli`, which is our default case.
    
    This new top-level binary could handle the following scenarios:
    
    1) On the creation of new CLIs, for instance, a new CLI called chef-analyze that
    will provide some analyze capabilities, as a user, you can run the binary directly
    chef-analyze foo or through our main Chef CLI wrapper chef analyze foo.
    Both ways would be running the same underlying binary.
    
    2) Redirecting existing binaries to a single point for further improments, this
    could be the case of improving, for example, our knife tool that could take a few
    seconds to run simple API searches like 'knife node list'  or 'knife cookbook list'
    
    3) On replacements of current functionality, for example, if we would like to improve
    the existing chef generate foo command, we would create a binary called chef-generate
    that users can run individually like chef-generate foo and then redirect the top-level
    CLI to run that same binary on any 'chef generate XYZ' execution.
    
    TODO @afiune design a better way to isolate single sub-commands that we want to
    replace/improve, for instance we might not want to rewrite the entire 'chef generate'
    sub-command but a section of it.
    
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/chef-cmd-wrapper/main.go
+++ components/chef-cmd-wrapper/main.go
@@ -18,9 +18,40 @@ func main() {
 		cmd        *exec.Cmd
 	)
 
-	// At the very beginning we want to pass every sub-command to the old
-	// 'chef' CLI binary that was renamed to 'chef-cli`
+	// At the very beginning we want to pass every sub-command to the old 'chef'
+	// CLI binary that was renamed to 'chef-cli`, which is our default case.
 	switch subCommand {
+	// @afiune Adding a few example of the usability of this new top-level binary:
+	//
+	// 1) On the creation of new CLIs, for instance, a new CLI called chef-analyze that
+	// will provide some analyze capabilities, as a user, you can run the binary directly
+	// chef-analyze foo or through our main Chef CLI wrapper chef analyze foo.
+	// Both ways would be running the same underlying binary.
+	case "analyze":
+		cmd = exec.Command("chef-analyze", allArgs...)
+
+	// 2) Redirecting existing binaries to a single point for further improments, this
+	// could be the case of improving, for example, our knife tool that could take a few
+	// seconds to run simple API searches like 'knife node list'  or 'knife cookbook list'
+	case "knife":
+		cmd = exec.Command("knife", allArgs...)
+
+	// 3) On replacements of current functionality, for example, if we would like to improve
+	// the existing chef generate foo command, we would create a binary called chef-generate
+	// that users can run individually like chef-generate foo and then redirect the top-level
+	// CLI to run that same binary on any 'chef generate XYZ' execution.
+	case "generate":
+		// TODO @afiune design a better way to isolate single sub-commands that we want to
+		// replace/improve, for instance we might not want to rewrite the entire 'chef generate'
+		// sub-command but a section of it.
+		//
+		// Example of a sub-command overwrite
+		if len(os.Args) > 2 && os.Args[2] == "build-cookbook" {
+			fmt.Println("The sub-command 'chef generate build-cookbook' has been deprecated.")
+			os.Exit(0)
+		}
+		fallthrough
+
 	default:
 		// When we land in the default case where we run the old 'chef' cli binary,
 		// we need to send the sub-command as well as all the arguments.
