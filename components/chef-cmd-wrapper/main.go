bb24fc53ab31db359b86c32c252ed6a4dc509192
Salim Afiune <afiune@chef.io>
2019-09-24 14:39:29 -0600

    Adds usage for experimental sub-commands
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- components/chef-cmd-wrapper/main.go
+++ components/chef-cmd-wrapper/main.go
@@ -86,6 +86,29 @@ func usage() {
     chef -v/--version
     chef command [arguments...] [options...]
 
+Available Commands:
+    exec                    Runs the command in context of the embedded ruby
+    env                     Prints environment variables used by Chef Workstation
+    gem                     Runs the 'gem' command in context of the embedded Ruby
+    generate                Generate a new repository, cookbook, or other component
+    shell-init              Initialize your shell to use Chef Workstation as your primary Ruby
+    install                 Install cookbooks from a Policyfile and generate a locked cookbook set
+    update                  Updates a Policyfile.lock.json with latest run_list and cookbooks
+    push                    Push a local policy lock to a policy group on the Chef Infra Server
+    push-archive            Push a policy archive to a policy group on the Chef Infra Server
+    show-policy             Show policyfile objects on the Chef Infra Server
+    diff                    Generate an itemized diff of two Policyfile lock documents
+    export                  Export a policy lock as a Chef Infra Zero code repo
+    clean-policy-revisions  Delete unused policy revisions on the Chef Infra Server
+    clean-policy-cookbooks  Delete unused policyfile cookbooks on the Chef Infra Server
+    delete-policy-group     Delete a policy group on the Chef Infra Server
+    delete-policy           Delete all revisions of a policy on the Chef Infra Server
+    undelete                Undo a delete command
+    describe-cookbook       Prints cookbook checksum information used for cookbook identifier
+
+Available Experimental Commands:
+    chef-analyse            Analyze your Chef inventory
+    knife                   The old school knife command
 `)
 }
 
