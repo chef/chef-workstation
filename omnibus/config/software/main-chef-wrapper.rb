a526767eedf2b0f19b3690e1ae2487d753463ec2
Salim Afiune <afiune@chef.io>
2019-10-30 14:56:24 -0600

    Add a meaningful comment to ruby block
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- omnibus/config/software/main-chef-wrapper.rb
+++ omnibus/config/software/main-chef-wrapper.rb
@@ -31,8 +31,8 @@ build do
   if windows?
     # Windows systems requires an extention (EXE)
     command "#{install_dir}/embedded/go/bin/go build -o #{install_dir}/bin/chef.exe", env: env
-    block do
-      # Generate a 'chef' file that redirects to the 'chef.exe' executable
+
+    block "Generate a 'chef' binary that calls the 'chef.exe' executable" do
       File.open("#{install_dir}/bin/chef", "w") do |f|
         f.write("@ECHO OFF\n\"%~dpn0.exe\" %*")
       end
