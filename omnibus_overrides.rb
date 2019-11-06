180a941271e6912b0f023ecf92ed0abcf45bbec0
Chef Expeditor <expeditor@chef.io>
2019-11-06 01:22:11 +0000

    Bump chef-analyze CLI to 0.1.21
    This pull request was triggered automatically via Expeditor when chef-analyze 0.1.21 was merged to master.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -5,7 +5,7 @@
 # commit / build through.
 
 override "delivery-cli", version: "0.0.54"
-override "chef-analyze", version: "0.1.20"
+override "chef-analyze", version: "0.1.21"
 override "chef-workstation-app", version: "v0.1.37"
 # /DO NOT MODIFY
 
