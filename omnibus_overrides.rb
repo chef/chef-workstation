25907ff52a0bec273043feec9f2067d8d924448b
Chef Expeditor <expeditor@chef.io>
2020-01-23 16:47:33 +0000

    Bump Chef Workstation App to 0.1.57
    This pull request was triggered automatically via Expeditor when Chef Workstation App 0.1.57 was merged.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@
 
 override "chef-analyze", version: "0.1.68"
 override "delivery-cli", version: "0.0.54"
-override "chef-workstation-app", version: "v0.1.56"
+override "chef-workstation-app", version: "v0.1.57"
 # /DO NOT MODIFY
 
 override :rubygems, version: "3.0.3" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which results in performance issues / CLI warnings. Make sure these versions match before bumping either.
