381616daca930d1c07b114a69f40baf04f67bccb
tyler-ball <tball@chef.io>
2020-03-09 22:45:42 -0700

    Bumping deps to latest version
    Signed-off-by: tyler-ball <tball@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -4,9 +4,9 @@
 # Expeditor takes that version, runs a script to replace it here and pushes a new
 # commit / build through.
 
-override "chef-analyze", version: "0.1.76"
+override "chef-analyze", version: "0.1.77"
 override "delivery-cli", version: "0.0.54"
-override "chef-workstation-app", version: "v0.1.74"
+override "chef-workstation-app", version: "v0.1.77"
 # /DO NOT MODIFY
 
 override :rubygems, version: "3.0.3" # rubygems ships its own bundler which may differ from bundler defined below and then we get double bundler which results in performance issues / CLI warnings. Make sure these versions match before bumping either.
