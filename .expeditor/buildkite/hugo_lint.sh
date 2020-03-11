e486ccad16b3f0e8d820aa82ce8279e74012efce
IanMadd <imaddaus@chef.io>
2020-03-11 15:41:58 -0700

    Update hugo_lint.sh script
    Signed-off-by: IanMadd <imaddaus@chef.io>

--- .expeditor/buildkite/hugo_lint.sh
+++ .expeditor/buildkite/hugo_lint.sh
@@ -2,14 +2,14 @@
 
 set -euo pipefail
 
-LINT_STATUS="$(grep -r -I --color=auto -o --with-filename -n -P '[^\x00-\x7F]' ./www/site/content/docs &> /dev/null ; echo $?)"
+LINT_STATUS="$(grep -r -I --color=auto -o --with-filename -n -P '[^\x00-\x7F]' ./www/content &> /dev/null ; echo $?)"
 
 if [ "$LINT_STATUS" == 1 ]; then
   echo "Success!"
   exit 0
 else
   echo "Failure!"
-  grep -r -I --color=auto -o --with-filename -n -P '[^\x00-\x7F]' ./www/site/content/docs
+  grep -r -I --color=auto -o --with-filename -n -P '[^\x00-\x7F]' ./www/content
   if [ "$LINT_STATUS" == 0 ]; then
     exit 1
   else
