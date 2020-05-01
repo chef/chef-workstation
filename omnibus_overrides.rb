ed94be71ab5292df94cf6ba7f846c862a585be3a
tyler-ball <tball@chef.io>
2020-04-30 19:04:56 -0700

    Updating to the latest chef-workstation-app
    Signed-off-by: tyler-ball <tball@chef.io>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@
 
 override "chef-analyze", version: "0.1.85"
 override "delivery-cli", version: "0.0.54"
-override "chef-workstation-app", version: "v0.1.77"
+override "chef-workstation-app", version: "v0.1.78"
 # /DO NOT MODIFY
 
 override "libarchive", version: "3.4.2"
