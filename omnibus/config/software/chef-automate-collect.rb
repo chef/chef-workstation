cf06b612b3a1305fd459224aa97e61271ea06874
Mudassar Shafique <15931574+mudash@users.noreply.github.com>
2020-08-26 13:25:24 -0400

    omnibus changes
    Signed-off-by: Mudassar Shafique <15931574+mudash@users.noreply.github.com>

---
+++ omnibus/config/software/chef-automate-collect.rb
@@ -0,0 +1,42 @@
+#
+# Copyright 2019 Chef Software, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This will be our rollout client for publishing policy changes
+#
+name "chef-automate-collect"
+source path: File.join("#{project.files_path}", "../../components/chef-automate-collect")
+license :project_license
+
+dependency "go"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+  env["CGO_ENABLED"] = "0"
+
+  if windows?
+    # Windows systems requires an extention (EXE)
+    command "#{install_dir}/embedded/go/bin/go build -o #{install_dir}/bin/chef-automate-collect.exe", env: env
+
+    block "Generate a 'chef-automate-collect' binary that calls the 'chef-automate-collect.exe' executable" do
+      File.open("#{install_dir}/bin/chef-automate-collect", "w") do |f|
+        f.write("@ECHO OFF\n\"%~dpn0.exe\" %*")
+      end
+    end
+  else
+    # Unix systems has no extention
+    command "#{install_dir}/embedded/go/bin/go build -o #{install_dir}/bin/chef-automate-collect", env: env
+  end
+end
