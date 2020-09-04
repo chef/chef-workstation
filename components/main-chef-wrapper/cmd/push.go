d5de1347605e6206e88477bf23fb23d8a410448c
Marc A. Paradise <marc.paradise@gmail.com>
2020-09-04 14:02:36 -0400

    CLI consistency - use spf13/cobra for main-chef-wrapper
    This updates main-chef-wrapper to route commands using cobra
    instead of a custom switch/case handler.
    
    This offers a few advantages for upcoming work:
    
    - auto support for shell autocomplete through 'cobra' means we can
      remove that from `chef-cli`, which will greatly increase the startup
      time of the Workstation Powershell on Windows.
    - viewing detailed help for any given command is now a fast operation
    because we don't have to load ruby VM deps
    - it will let us replace and/or expand commands in `chef-cli` in an
    isolated fashion, because each command has its own implementation. (Most
    implementations current pass through to chef-cli or chef-analyze)
    
    Signed-off-by: Marc A. Paradise <marc.paradise@gmail.com>

---
+++ components/main-chef-wrapper/cmd/push.go
@@ -0,0 +1,52 @@
+//
+// Copyright 2020 Chef Software, Inc.
+//
+// Author: Marc A. Paradise <marc.paradise@gmail.com>
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// LIMITATIONS UNDER THE LICENSE.
+//
+
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/chef/chef-workstation/components/main-chef-wrapper/dist"
+	"github.com/spf13/cobra"
+)
+
+var pushCmd = &cobra.Command{
+	Use:   "push POLICY_GROUP [ POLICY_FILE ]",
+	Short: "Push a local policyfile lock to a policy group on the %s",
+	Long: `
+Upload an existing Policyfile.lock.json to a %s, along
+with all the cookbooks contained in the policy lock. The policy lock is applied
+to a specific POLICY_GROUP, which is a set of nodes that share the same
+run_list and cookbooks.
+
+See the Policyfile documentation for more information:
+
+https://docs.chef.io/policyfile/
+`,
+	FParseErrWhitelist: cobra.FParseErrWhitelist{UnknownFlags: true},
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return passThroughCommand(dist.WorkstationExec, "", os.Args[1:])
+	},
+}
+
+func init() {
+	pushCmd.Short = fmt.Sprintf(pushCmd.Short, dist.ServerProduct)
+	pushCmd.Long = fmt.Sprintf(pushCmd.Long, dist.ServerProduct)
+	rootCmd.AddCommand(pushCmd)
+}
--- components/main-chef-wrapper/go.mod
+++ components/main-chef-wrapper/go.mod
@@ -7,4 +7,5 @@ require (
 	github.com/mitchellh/go-homedir v1.1.0
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/stretchr/testify v1.6.1
+	github.com/spf13/cobra v1.0.0
 )
