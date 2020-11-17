c46adbd72ed35263e1666a50e5d20803e4b55571
Chef Expeditor <chef-ci@chef.io>
2020-11-17 04:04:42 +0000

    Bump Chef Workstation App to 0.1.93
    This pull request was triggered automatically via Expeditor when Chef Workstation App 0.1.93 was merged.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@
 
 override "chef-analyze", version: "0.1.101"
 override "delivery-cli", version: "0.0.54"
-override "chef-workstation-app", version: "v0.1.91"
+override "chef-workstation-app", version: "v0.1.93"
 # /DO NOT MODIFY
 
 override "libarchive", version: "3.4.3"
