d054f3eddce8c8e56c69fad02d9f259b24ac5925
Collin McNeese <cmcneese@chef.io>
2020-11-20 09:46:38 -0600

    Updates included Habitat to 1.6.181 from 1.6.56
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- omnibus/config/software/habitat.rb
+++ omnibus/config/software/habitat.rb
@@ -15,29 +15,29 @@
 # limitations under the License.
 #
 
-name "habitat"
+name 'habitat'
 license :project_license
 skip_transitive_dependency_licensing true
 
 # DO NOT MODIFY: default_version and checksums are populated by
 #                workstation/.expeditor/update_habitat.sh
-default_version "1.6.56"
-linux_sha = "a87f4ff7558f23724289e2c5a9b75920da364818167f63b26411be5a8b344800"
-darwin_sha = "42b6c417b88351e0b8ce99cb6ebcc104bbb5bc819ffc795f980d1201068035e6"
-windows_sha = "648157f1db680233b676725d3142ef118e5a77da000209a532fb54ff153b57dd"
+default_version '1.6.181'
+linux_sha = 'efa91792f9d116dd63c481b3d3ee88e65e0cb49f0654578fc93166f614590809'
+darwin_sha = '268c149431842a58cdbdcf3e7354f90dec38cd548568520b8a602e48d8d4cbb8'
+windows_sha = '097da90435458a2ac2739e12c63bfba55bb4c7ba163b2ae65ab93968e9ced8c3'
 # END DO NOT MODIFY
 
 if windows?
-  suffix = "windows.zip"
+  suffix = 'windows.zip'
   sha256 = windows_sha
 elsif linux?
-  suffix = "linux.tar.gz"
+  suffix = 'linux.tar.gz'
   sha256 = linux_sha
 elsif mac?
-  suffix = "darwin.zip"
+  suffix = 'darwin.zip'
   sha256 = darwin_sha
 else
-  raise "habitat dep is only available for windows, linux, and mac"
+  raise 'habitat dep is only available for windows, linux, and mac'
 end
 
 source url: "https://packages.chef.io/files/stable/habitat/#{version}/hab-x86_64-#{suffix}",
@@ -46,8 +46,8 @@ source url: "https://packages.chef.io/files/stable/habitat/#{version}/hab-x86_64
 build do
   # 'block' is needed to prevent evaluating the ruby code
   # before the project_dir contains the extracted package.
-  block "Relocating habitat" do
-    dest = File.join(install_dir, "bin")
+  block 'Relocating habitat' do
+    dest = File.join(install_dir, 'bin')
     # We don't just copy the bin itself because on Windows additional
     # supporting DLLs are included.
     Dir.glob("#{project_dir}/hab-*/*").each do |f|
