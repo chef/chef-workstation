748b9c0f609dec1fb0c2051871cd3769b85eb6c8
Collin McNeese <cmcneese@chef.io>
2020-11-20 09:53:31 -0600

    reverts to double-quoted strings in omnibus/config/software/habitat.rb
    Signed-off-by: Collin McNeese <cmcneese@chef.io>

--- omnibus/config/software/habitat.rb
+++ omnibus/config/software/habitat.rb
@@ -15,40 +15,40 @@
 # limitations under the License.
 #
 
-name 'habitat'
+name "habitat"
 license :project_license
 skip_transitive_dependency_licensing true
 
 # DO NOT MODIFY: default_version and checksums are populated by
 #                workstation/.expeditor/update_habitat.sh
-default_version '1.6.181'
-linux_sha = 'efa91792f9d116dd63c481b3d3ee88e65e0cb49f0654578fc93166f614590809'
-darwin_sha = '268c149431842a58cdbdcf3e7354f90dec38cd548568520b8a602e48d8d4cbb8'
-windows_sha = '097da90435458a2ac2739e12c63bfba55bb4c7ba163b2ae65ab93968e9ced8c3'
+default_version "1.6.181"
+linux_sha = "efa91792f9d116dd63c481b3d3ee88e65e0cb49f0654578fc93166f614590809"
+darwin_sha = "268c149431842a58cdbdcf3e7354f90dec38cd548568520b8a602e48d8d4cbb8"
+windows_sha = "097da90435458a2ac2739e12c63bfba55bb4c7ba163b2ae65ab93968e9ced8c3"
 # END DO NOT MODIFY
 
 if windows?
-  suffix = 'windows.zip'
+  suffix = "windows.zip"
   sha256 = windows_sha
 elsif linux?
-  suffix = 'linux.tar.gz'
+  suffix = "linux.tar.gz"
   sha256 = linux_sha
 elsif mac?
-  suffix = 'darwin.zip'
+  suffix = "darwin.zip"
   sha256 = darwin_sha
 else
-  raise 'habitat dep is only available for windows, linux, and mac'
+  raise "habitat dep is only available for windows, linux, and mac"
 end
 
 source url: "https://packages.chef.io/files/stable/habitat/#{version}/hab-x86_64-#{suffix}",
   sha256: sha256
 
 build do
-  # 'block' is needed to prevent evaluating the ruby code
+  # "block" is needed to prevent evaluating the ruby code
   # before the project_dir contains the extracted package.
-  block 'Relocating habitat' do
-    dest = File.join(install_dir, 'bin')
-    # We don't just copy the bin itself because on Windows additional
+  block "Relocating habitat" do
+    dest = File.join(install_dir, "bin")
+    # We don"t just copy the bin itself because on Windows additional
     # supporting DLLs are included.
     Dir.glob("#{project_dir}/hab-*/*").each do |f|
       copy f, dest
