1f4392df81992c3b3290b029b649c4c3fcd78d90
Tim Smith <tsmith84@gmail.com>
2020-12-21 18:06:09 -0800

    Remove another push client reference
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- omnibus/config/software/gems.rb
+++ omnibus/config/software/gems.rb
@@ -77,7 +77,7 @@ build do
   appbundle "berkshelf", lockdir: project_dir, gem: "berkshelf", without: %w{changelog build docs debug development}, env: env
 
   # Note - 'chef-apply' gem provides 'chef-run', not 'chef-apply' which ships with chef-bin...
-  %w{chef-bin chef-apply chef-vault ohai opscode-pushy-client cookstyle}.each do |gem|
+  %w{chef-bin chef-apply chef-vault ohai cookstyle}.each do |gem|
     appbundle gem, lockdir: project_dir, gem: gem, without: %w{changelog}, env: env
   end
 
