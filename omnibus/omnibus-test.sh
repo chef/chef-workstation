de7885a57c76b2461cd740459693c1850338de5f
Tom Duffield <github@tomduffield.com>
2021-04-23 15:49:28 -0500

    Remove pipeline-specific logic from test scripts
    This logic has been moved into the Omnibus Buildkite Plugin.
    These scripts can now be used outside of the Buildkite pipelines.
    
    Signed-off-by: Tom Duffield <github@tomduffield.com>

--- omnibus/omnibus-test.sh
+++ omnibus/omnibus-test.sh
@@ -1,41 +1,10 @@
 #!/bin/bash
 set -ueo pipefail
 
-channel="${CHANNEL:-unstable}"
-product="${PRODUCT:-chef-workstation}"
-version="${VERSION:-latest}"
-package_file=${PACKAGE_FILE:-""}
-
 is_darwin()
 {
   uname -a | grep "^Darwin" 2>&1 >/dev/null
 }
-
-echo "--- Installing $channel $product $version"
-if [[ -z $package_file ]]; then
-  package_file="$(.omnibus-buildkite-plugin/install-omnibus-product.sh -c "$channel" -P "$product" -v "$version" | tail -1)"
-else
-  .omnibus-buildkite-plugin/install-omnibus-product.sh -f "$package_file" -P "$product" -v "$version" &> /dev/null
-fi
-
-echo "--- Verifying omnibus package is signed"
-/opt/omnibus-toolchain/bin/check-omnibus-package-signed "$package_file"
-
-sudo rm -f "$package_file"
-
-echo "--- Verifying ownership of package files"
-
-export INSTALL_DIR=/opt/chef-workstation
-NONROOT_FILES="$(find "$INSTALL_DIR" ! -user 0 -print)"
-if [[ "$NONROOT_FILES" == "" ]]; then
-  echo "Packages files are owned by root.  Continuing verification."
-else
-  echo "Exiting with an error because the following files are not owned by root:"
-  echo "$NONROOT_FILES"
-  exit 1
-fi
-
-echo "--- Running verification for $channel $product $version"
 
 # Ensure user variables are set in git config
 git config --global user.email "you@example.com"
