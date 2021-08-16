e2b051d20ad26fd6b4108c1a410eab661e47061a
Chef Expeditor <chef-ci@chef.io>
2021-08-16 19:02:41 +0000

    Bump Chef Workstation App to 0.2.42
    This pull request was triggered automatically via Expeditor when Chef Workstation App 0.2.42 was merged.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus/config/software/chef-workstation-app.rb
+++ omnibus/config/software/chef-workstation-app.rb
@@ -21,9 +21,9 @@ skip_transitive_dependency_licensing
 license_file "LICENSE"
 
 # These three lines are updated automatically by Expeditor
-default_version "0.2.40"
-source sha1: "b9a74a9001a5e4f70906cebb3526bffa9eb953f8" if windows?
-source sha1: "c4eb210b4abb00a6bb673c1e35645c7e068e76c4" if linux?
+default_version "0.2.42"
+source sha1: "480d0eb9fd44aa7476347cc51d8b946b2b1c5a4d" if windows?
+source sha1: "aa8dd96f079aaf3cad7caf62752e5988437264fb" if linux?
 
 platform_name = if macos?
                   "darwin"
