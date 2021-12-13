d4bd30d11306a36f006920dcddc4e61933bda3a7
Pranay Singh <i5singh.pranay@gmail.com>
2021-12-13 18:30:58 +0530

    checking for env file and calling ruby script based on boolean flag
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- components/main-chef-wrapper/main.go
+++ components/main-chef-wrapper/main.go
@@ -19,13 +19,18 @@ limitations under the License.
 package main
 
 import (
+	"fmt"
+	"log"
 	"os"
 
 	"github.com/chef/chef-workstation/components/main-chef-wrapper/cmd"
 	homedir "github.com/mitchellh/go-homedir"
+	platform_lib "github.com/chef/chef-workstation/components/main-chef-wrapper/platform-lib"
+
 )
 
 func doStartupTasks() error {
+	createRubyEnv()
 	createDotChef()
 	return nil
 }
@@ -40,6 +45,33 @@ func createDotChef() {
 		return
 	}
 	os.Mkdir(path, 0700)
+}
+
+func createRubyEnv(){
+//	get installation path
+//	homepath, err := homedir.Dir()
+//	if err != nil {
+//		log.Fatalf(err.Error())
+//	}
+//	todo ==> incase home directory is needed we can add it to string
+	installationPath :=  platform_lib.ExpectedOmnibusRoot() + "/ruby-env.json"
+	fmt.Printf(installationPath)
+	result, err := exists(installationPath)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	if result == true {
+		fmt.Print("file exists======== ruby script not needed")
+	} else {
+		fmt.Print("file  does not exists============ call ruby script to make ruby-env.json file\n")
+	}
+}
+
+func exists(path string) (bool, error) {
+	_, err := os.Stat(path)
+	if err == nil { return true, nil }
+	if os.IsNotExist(err) { return false, nil }
+	return false, err
 }
 
 func main() {
