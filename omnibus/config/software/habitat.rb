f6e02d3af6189d29b5b10ed95166456857c5b05e
Ashwin <avarma@msystechnologies.com>
2021-12-22 15:29:27 +0530

    using arm? helper from the architecture
    Signed-off-by: Ashwin <avarma@msystechnologies.com>

--- omnibus/config/software/habitat.rb
+++ omnibus/config/software/habitat.rb
@@ -35,7 +35,7 @@ elsif linux?
   suffix = "x86_64-linux.tar.gz"
   sha256 = linux_sha
 elsif mac? 
-  if /arm64/ =~ RUBY_PLATFORM
+  if arm?
     suffix = "aarch64-darwin.zip"
     sha256 = darwin_m1_sha
   else
