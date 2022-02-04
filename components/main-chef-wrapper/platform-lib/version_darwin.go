4e9aa044245aa2924eceb7a26a3facbb1c3a4fbd
Pranay Singh <i5singh.pranay@gmail.com>
2022-02-04 13:17:59 +0530

    making version ready for build mode after cherrypicking
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- components/main-chef-wrapper/platform-lib/version_darwin.go
+++ components/main-chef-wrapper/platform-lib/version_darwin.go
@@ -18,11 +18,13 @@ package platform_lib
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/chef/chef-workstation/components/main-chef-wrapper/dist"
-	"github.com/chef/chef-workstation/components/main-chef-wrapper/lib"
 	"io/ioutil"
 	"os"
 	"path"
+	"path/filepath"
+
+	"github.com/chef/chef-workstation/components/main-chef-wrapper/dist"
+	"github.com/chef/chef-workstation/components/main-chef-wrapper/lib"
 )
 
 var gemManifestMap map[string]interface{}
@@ -117,33 +119,33 @@ func OmnibusInstall() bool {
 }
 
 func omnibusRoot() string {
-	//omnibusroot, err := filepath.Abs(path.Join(ExpectedOmnibusRoot()))
-	//if err != nil {
-	//	fmt.Fprintln(os.Stderr, "ERROR:", dist.WorkstationProduct, "has not been installed via the platform-specific package provided by", dist.DistributorName, "Version information is not available.")
-	//	os.Exit(4)
-	//}
-	//return omnibusroot
-	////below code can be used for running and testing in local repos e.g ./main-chef-wrapper -v, comment out rest code of this method(darwin,linux)
-	return "/opt/chef-workstation"
+	omnibusroot, err := filepath.Abs(path.Join(ExpectedOmnibusRoot()))
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR:", dist.WorkstationProduct, "has not been installed via the platform-specific package provided by", dist.DistributorName, "Version information is not available.")
+		os.Exit(4)
+	}
+	return omnibusroot
+	//below code can be used for running and testing in local repos e.g ./main-chef-wrapper -v, comment out rest code of this method(darwin,linux)
+	// return "/opt/chef-workstation"
 }
 
 func ExpectedOmnibusRoot() string {
-	//ex, _ := os.Executable()
-	//exReal, err := filepath.EvalSymlinks(ex)
-	//if err != nil {
-	//	fmt.Fprintln(os.Stderr, "ERROR:", err)
-	//	os.Exit(4)
-	//}
-	//rootPath := path.Join(filepath.Dir(exReal), "..")
-	////groot := os.Getenv("GEM_ROOT")
-	////rootPath, err := filepath.Abs(path.Join(groot,"..","..", "..", "..", ".."))
-	//return rootPath
-	////below code can be used for running and testing in local repos e.g ./main-chef-wrapper -v, comment out rest code of this method(darwin,linux)
-	return "/opt/chef-workstation"
+	ex, _ := os.Executable()
+	exReal, err := filepath.EvalSymlinks(ex)
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "ERROR:", err)
+		os.Exit(4)
+	}
+	rootPath := path.Join(filepath.Dir(exReal), "..")
+	//groot := os.Getenv("GEM_ROOT")
+	//rootPath, err := filepath.Abs(path.Join(groot,"..","..", "..", "..", ".."))
+	return rootPath
+	//below code can be used for running and testing in local repos e.g ./main-chef-wrapper -v, comment out rest code of this method(darwin,linux)
+	// return "/opt/chef-workstation"
 }
 
-func UnmarshallRubyEnv() map[string]interface{}{
-	filepath := path.Join( omnibusRoot(), "ruby-env.json")
+func UnmarshallRubyEnv() map[string]interface{} {
+	filepath := path.Join(omnibusRoot(), "ruby-env.json")
 	jsonFile, err := os.Open(filepath)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, "ERROR:", err.Error())
