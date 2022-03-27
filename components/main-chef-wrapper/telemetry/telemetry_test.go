189ecb89098036a88c6e3966c9d4891f341b8924
nikhil2611 <nikhilgupta2102@gmail.com>
2022-03-27 23:38:12 +0530

    added test cases
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- components/main-chef-wrapper/telemetry/telemetry_test.go
+++ components/main-chef-wrapper/telemetry/telemetry_test.go
@@ -2,18 +2,19 @@ package telemetry
 
 import (
 	"fmt"
+	_ "github.com/chef/go-libs/telemetry"
 	"reflect"
 	"testing"
 )
 
 func TestNewTelemetry(t *testing.T) {
 	type Telemetry struct {
-		Payload_dir                  string
-		Session_file                 string
-		Installation_identifier_file string
+		PayloadDir                  string
+		SessionFile                 string
+		InstallationIdentifierFile string
 		Enabled                      bool
-		Dev_mode                     bool
-		Host_os                      string
+		DevMode                     bool
+		HostOs                      string
 		Arch                         string
 		Version                      string
 	}
@@ -27,4 +28,40 @@ func TestNewTelemetry(t *testing.T) {
 	// if got != newData {
 	// 	t.Errorf("got %q, wanted %q", got, newData)
 	// }
+}
+
+
+func TestCreateDefaultConfig(t *testing.T) {
+	type Telemetry struct {
+		Payload_dir                  string
+		Session_file                 string
+		Installation_identifier_file string
+		Enabled                      bool
+		Dev_mode                     bool
+		Host_os                      string
+		Arch                         string
+		Version                      string
+	}
+
+	var newData = Telemetry{"/Users/ngupta/.chef-workstation/telemetry", "/Users/ngupta/.chef-workstation/telemetry/TELEMETRY_SESSION_ID", "/Users/ngupta/.chef-workstation/installation_id", true, false, "darwin", "amd64", "22.2.802"}
+	Got := NewTelemetry()
+	// want := 10
+	fmt.Println("got is------------", reflect.TypeOf(Got))
+	fmt.Println("newData is------------", reflect.TypeOf(newData))
+
+	// if got != newData {
+	// 	t.Errorf("got %q, wanted %q", got, newData)
+	// }
+}
+
+func TestStartupTelemetry(t *testing.T) {
+
+	err := FirstRunTask()
+	if err!= nil {
+		fmt.Println(err)
+	}
+	err1 := SetupWorkstationUserDirectories()
+	if err!= nil {
+		fmt.Println(err1)
+	}
 }
