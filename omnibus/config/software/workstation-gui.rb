a7b44046bc4a6d72dffcd483cb9c46daf233468c
Pranay Singh <i5singh.pranay@gmail.com>
2022-07-21 14:40:09 +0530

    Adding omnibus packaging to file path embedded/service/workstation-gui/
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

---
+++ omnibus/config/software/workstation-gui.rb
@@ -0,0 +1,44 @@
+#
+# Copyright:: Copyright Chef Software, Inc.
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+name "workstation-gui"
+license :project_license
+
+source path: File.join("#{project.files_path}", "../../src/workstation-gui")
+
+# todo need to checkout all the dependency
+dependency "ruby"
+
+build do
+  env = with_standard_compiler_flags(with_embedded_path)
+
+  bundle "package --no-install", env: env
+  # This statement is to replace the --without flag which is getting deprecated
+  bundle "config set without 'development doc'"
+  bundle "install" \
+         " --jobs #{workers}" \
+         " --retry 3" \
+         " --path=vendor/bundle",
+         env: env
+
+
+  # This fails because we're installing Ruby C extensions in the wrong place!
+  # bundle "exec rake assets:precompile", env: env # Note--> not needed as this is api only app
+  # todo - service folder is not present in embedded, will this create service, or do we have to put inside lib.
+  sync project_dir, "#{install_dir}/embedded/service/workstation-gui/",
+       exclude: %w( .env)
+end
