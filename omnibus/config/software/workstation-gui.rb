d302484ce1b1af69b42430c04819b1ffb79ecf7b
Pranay Singh <i5singh.pranay@gmail.com>
2022-07-28 12:32:22 +0530

    adding jobs and retry
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- omnibus/config/software/workstation-gui.rb
+++ omnibus/config/software/workstation-gui.rb
@@ -29,7 +29,10 @@ build do
   # bundle "package --no-install", env: env
   # This statement is to replace the --without flag which is getting deprecated
   # bundle "config set without 'development doc'"
-  bundle "install",
+  bundle "install" \
+         " --jobs #{workers}" \
+         " --retry 3" \
+         " --path=vendor/bundle",
          env: env
 
 
