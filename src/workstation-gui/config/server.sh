3f4a8579e3145f3e7fd1a97ea8370044172c5ce8
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-09 16:13:33 +0530

    update the bundle install path
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/config/server.sh
+++ src/workstation-gui/config/server.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
-bundle exec /opt/chef-workstation/embedded/lib/ruby/3.0.0/bin/puma -C /opt/chef-workstation/embedded/service/workstation-gui/config/puma.rb
+# /opt/chef-workstation/bin/puma
+
+puma -C /opt/chef-workstation/embedded/service/workstation-gui/config/puma.rb
