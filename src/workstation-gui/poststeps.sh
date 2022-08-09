38eae0e4d59657fbfd2823b73e72cf2d732f3983
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-09 16:13:33 +0530

    update the bundle install path
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
-echo "nikhil"
 
-echo "$PWD"
-cd /opt/chef-workstation/embedded/service/workstation-gui
+# echo "nikhil"
 
-echo "$PWD"
+# echo "$PWD"
+# cd /opt/chef-workstation/embedded/service/workstation-gui
 
-chmod 777 /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
+# echo "$PWD"
+
+chmod +x /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
 
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.workstation.plist ~/Library/LaunchAgents/
 
