6889881fe1dfd06caa897470870e796d01650a63
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-09 13:10:00 +0530

    starting server from script
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -6,8 +6,8 @@ cd /opt/chef-workstation/embedded/service/workstation-gui
 
 echo "$PWD"
 
+chmod 777 /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
+
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.workstation.plist ~/Library/LaunchAgents/
 
 launchctl load ~/Library/LaunchAgents/io.chef.workstation.plist
-
-launchctl start ~/Library/LaunchAgents/io.chef.workstation.plist
