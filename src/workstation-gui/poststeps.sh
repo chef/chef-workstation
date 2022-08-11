711926c2fcea004dd83068a1451578fadf090718
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-11 18:55:31 +0530

    remove the bundle config
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -7,13 +7,13 @@
 
 # echo "$PWD"
 
-mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/gems/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/gems
+# mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/gems/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/gems
 
-mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/extensions/x86_64-darwin-19/3.0.0/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/extensions/x86_64-darwin-19/3.0.0
+# mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/extensions/x86_64-darwin-19/3.0.0/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/extensions/x86_64-darwin-19/3.0.0
 
-mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/specifications/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/specifications
+# mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/specifications/* /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/specifications
 
-mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/bin/* /opt/chef-workstation/bin
+# mv /opt/chef-workstation/embedded/lib/ruby/gems/3.0.0/ruby/3.0.0/bin/* /opt/chef-workstation/bin
 
 chmod +x /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
 
