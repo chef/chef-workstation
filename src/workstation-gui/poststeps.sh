ba673c464397af2b9f035c094b6cfa0961d029fc
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-12 13:00:33 +0530

    added permisssion to tmp folder
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -17,6 +17,8 @@
 
 chmod +x /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
 
+chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp
+
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
 
 launchctl load ~/Library/LaunchAgents/io.chef.chef-workstation.plist
