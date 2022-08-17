5440123c884917f89fa32aa7eb45e832d0838bd1
Ashique P S <Ashique.saidalavi@progress.com>
2022-08-17 18:26:12 +0530

    Updated the poststeps file
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -6,8 +6,18 @@ cd /opt/chef-workstation/embedded/service/workstation-gui
 
 echo "$PWD"
 
+echo "Inside the poststeps" >> bashlog.txt
+# Setting up the permissions for the tmp and log directories
+chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp/
+chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/log/
+
+echo "Updated the persmissions for tmp and log" >> bashlog.txt
 # Regenerate the secrets
+rm config/credentials.yml.enc
+rm config/master.key
+echo "Deleted files" >> bashlog.txt
 rake secrets:regenerate
+echo "Regerated the secrets" >> bashlog.txt
 
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.workstation.plist ~/Library/LaunchAgents/
 
