978151ef5cc827053652f8b06fe186cf8042464e
nikhil2611 <nikhilgupta2102@gmail.com>
2022-08-26 15:57:21 +0530

    updated the database dependency
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/config/application.rb
+++ src/workstation-gui/config/application.rb
@@ -1,6 +1,10 @@
 require_relative "boot"
 
-require "rails/all"
+# require "rails/all"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "sprockets/railtie"
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
