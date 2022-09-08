1befa0f870264baa29e06a26c3de91a59a936a15
nikhil2611 <nikhilgupta2102@gmail.com>
2022-09-08 13:39:15 +0530

    added logs
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/config/win_server.bat
+++ src/workstation-gui/config/win_server.bat
@@ -1,5 +1,5 @@
 @echo off
-Write-Output "INSIDE THE win_server.bat file"
+Write-Output "inside win_server.bat file"
 cd C:\opscode\chef-workstation\embedded\service\workstation-gui
 C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\puma -C C:\opscode\chef-workstation\embedded\service\workstation-gui\config\puma.rb
-Write-Output "server started successfully"
+Write-Output "running the server succesfully"
