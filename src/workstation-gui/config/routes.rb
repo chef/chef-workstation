26fe095865a46f7338eb810da9565fd3287327fb
nikhil2611 <nikhilgupta2102@gmail.com>
2022-09-12 11:38:36 +0530

    added the routs and handled error
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/config/routes.rb
+++ src/workstation-gui/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "policies#push"
-  # resources :policies, only: [:create]
+  # post "policies/push", to: "policies#push"
+  post "policies/create"
   
 end
