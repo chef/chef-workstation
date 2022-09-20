a8dad1b7f0894f5fdd295bc8305ba81e51a43af8
Ashique P S <Ashique.saidalavi@progress.com>
2022-09-20 17:41:35 +0530

    Puma daemonize and dotenv integration
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/config/puma.rb
+++ src/workstation-gui/config/puma.rb
@@ -1,9 +1,12 @@
+require "dotenv"
+require "puma/daemon"
 # Puma can serve each request in a thread from an internal thread pool.
 # The `threads` method setting takes two numbers: a minimum and maximum.
 # Any libraries that use thread pools should be configured to match
 # the maximum value specified for Puma. Default is set to 5 threads for minimum
 # and maximum; this matches the default thread size of Active Record.
 #
+Dotenv.load
 max_threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 min_threads_count = ENV.fetch("RAILS_MIN_THREADS") { max_threads_count }
 threads min_threads_count, max_threads_count
@@ -15,7 +18,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-port ENV.fetch("PORT") { 3000 }
+port ENV.fetch("CHEF_WS_API_PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
@@ -41,3 +44,6 @@ pidfile ENV.fetch("PIDFILE") { "tmp/pids/server.pid" }
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
+
+# Run puma process as a daemon
+daemonize true
--- src/workstation-gui/config/win_server.bat
+++ src/workstation-gui/config/win_server.bat
@@ -1,5 +1,5 @@
 @echo off
 Write-Output "inside win_server.bat file"
 cd C:\opscode\chef-workstation\embedded\service\workstation-gui
-C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\puma -C C:\opscode\chef-workstation\embedded\service\workstation-gui\config\puma.rb
+C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\pumad -C C:\opscode\chef-workstation\embedded\service\workstation-gui\config\puma.rb
 Write-Output "running the server succesfully"
