bd350fb04e5193314d7374e47ea11c3933846bfc
Ashique P S <Ashique.saidalavi@progress.com>
2022-09-22 14:31:24 +0530

    Cleanup
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/config/win_server.bat
+++ src/workstation-gui/config/win_server.bat
@@ -1,5 +1,3 @@
 @echo off
-Write-Output "inside win_server.bat file"
 cd C:\opscode\chef-workstation\embedded\service\workstation-gui
 C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\puma -C C:\opscode\chef-workstation\embedded\service\workstation-gui\config\puma.rb
-Write-Output "running the server succesfully"
--- src/workstation-gui/server.ps1
+++ src/workstation-gui/server.ps1
@@ -1,4 +1,3 @@
-Write-Output "INSIDE THE server.ps1 file"
 $access_key = -join ((48..57) + (97..122) | Get-Random -Count 16 | % {[char]$_})
 
 $ws_path = "C:\opscode\chef-workstation"
@@ -6,7 +5,6 @@ $server_path = Join-Path $ws_path "embedded\service\workstation-gui" -Resolve
 
 Set-Location -Path $ws_path
 Add-Content -Path "service.txt" -Value $access_key
-Write-Output "Created the service.txt file"
 
 Set-Location -Path $server_path
 
@@ -19,11 +17,8 @@ if(Test-Path config\master.key)
 {
   Remove-Item config\master.key
 }
-Write-Output "Before running the rake task"
 C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\rake secrets:regenerate["$access_key"]
 
-Write-Output "After the rake task"
 Set-Location -Path "config"
 Start-Process -windowstyle Hidden -FilePath "win_server.bat"
 
-Write-Output "After starting the rails server"
