2024a6e824b421a417bccd9dea2776ae5002619a
Ashique P S <Ashique.saidalavi@progress.com>
2022-09-22 14:29:16 +0530

    Fixed the issue with secrets regenration on macos
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
-
-chmod +x /opt/chef-workstation/embedded/service/workstation-gui/config/server.sh
+chmod +x /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 
 chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp
+
+bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
 
