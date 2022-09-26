884c0e982efdca635230fb84f2a47fb8fcaca53b
Ashique P S <Ashique.saidalavi@progress.com>
2022-09-26 22:24:33 +0530

    Updated with correct path
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/server.ps1
+++ src/workstation-gui/server.ps1
@@ -1,13 +1,11 @@
 $access_key = -join ((48..57) + (97..122) | Get-Random -Count 16 | % {[char]$_})
 
-$ws_path = "C:\opscode\chef-workstation"
+$ws_path = (split-path $MyInvocation.MyCommand.Definition -Parent) | Split-Path | Split-Path | Split-Path
+Add-Content -Path "c:\opscode\chef-workstation\sample.txt" -Value $ws_path
 $server_path = Join-Path $ws_path "embedded\service\workstation-gui" -Resolve
 
 Set-Location -Path $ws_path
 Add-Content -Path "service.txt" -Value $access_key
-
-$chefws_bin = (split-path $MyInvocation.MyCommand.Definition -Parent) | Split-Path
-Add-Content -Path "sample.txt" -Value $chefws_bin
 
 Set-Location -Path $server_path
 
