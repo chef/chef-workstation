7bee71b4dee20b0e6bc471d51ad32e1247ea660e
Ashique P S <Ashique.saidalavi@progress.com>
2022-10-06 21:00:42 +0530

    Fix for service not starting after system restart
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/config/win_server.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd C:\opscode\chef-workstation\embedded\service\workstation-gui
-C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\puma -C C:\opscode\chef-workstation\embedded\service\workstation-gui\config\puma.rb
--- src/workstation-gui/server.ps1
+++ src/workstation-gui/server.ps1
@@ -1,10 +1,11 @@
 $access_key = -join ((48..57) + (97..122) | Get-Random -Count 16 | % {[char]$_})
 
 $ws_path = (split-path $MyInvocation.MyCommand.Definition -Parent) | Split-Path | Split-Path | Split-Path
+$bin_path = Join-Path $ws_path "embedded\bin" -Resolve
 $server_path = Join-Path $ws_path "embedded\service\workstation-gui" -Resolve
 
 Set-Location -Path $ws_path
-Add-Content -Path "service.txt" -Value $access_key
+Set-Content -Path "service.txt" -Value $access_key
 
 Set-Location -Path $server_path
 
@@ -17,8 +18,9 @@ if(Test-Path config\master.key)
 {
   Remove-Item config\master.key
 }
-C:\opscode\chef-workstation\embedded\bin\bundle exec C:\opscode\chef-workstation\embedded\bin\rake secrets:regenerate["$access_key"]
 
-Set-Location -Path "config"
-Start-Process -windowstyle Hidden -FilePath "win_server.bat"
+$regenerate_cmd = "$bin_path\bundle exec $bin_path\rake secrets:regenerate['$access_key']"
+Invoke-Expression $regenerate_cmd
 
+$puma_cmd = "$bin_path\bundle exec $bin_path\puma -C $server_path\config\puma.rb"
+Invoke-Expression $puma_cmd
