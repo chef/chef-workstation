ba2b191f5bda0707e7c99d92fad10152449afdf8
Ashique P S <Ashique.saidalavi@progress.com>
2022-10-12 14:04:26 +0530

    Fixed the issue with permissions on log directory
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -3,6 +3,8 @@ chmod +x /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 
 chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp
 
+chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/log
+
 bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
