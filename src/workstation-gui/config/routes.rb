77136bc57bf070bc49a4445711f1396f93f4d151
nikhil2611 <nikhilgupta2102@gmail.com>
2022-10-18 17:54:16 +0530

    added notarize fix and post route to link repo
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- src/workstation-gui/config/routes.rb
+++ src/workstation-gui/config/routes.rb
@@ -11,7 +11,7 @@ Rails.application.routes.draw do
 
       resource :cookbook, only: [:create]
       get "repositories/list_repositories", to: "repositories#repositories"
-      get "repositories/link_repository", to: "repositories#link_repository" # todo - this is post call, but only get call is working in app so change later
+      post "repositories/link_repository", to: "repositories#link_repository" # todo - this is post call, but only get call is working in app so change later
       get "repositories/cookbooks", to: "cookbooks#cookbooks"
       get "cookbook/recipes", to: "recipes#recipes"
     end
--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -9,4 +9,4 @@ bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 
 cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
 
-launchctl load ~/Library/LaunchAgents/io.chef.chef-workstation.plist
+sudo -u $SUDO_USER launchctl load ~/Library/LaunchAgents/io.chef.chef-workstation.plist
