f67b00c4ad439d6ce973b69a264193b979d365e5
nikhil2611 <nikhilgupta2102@gmail.com>
2022-10-18 10:53:16 +0530

    added dependency
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/workstation-gui.rb
+++ omnibus/config/software/workstation-gui.rb
@@ -22,10 +22,10 @@ source path: File.join("#{project.files_path}", "../../src/workstation-gui")
 
 # todo need to checkout all the dependency
 dependency "ruby"
-# dependency "libxml2"
-# dependency "libxslt"
-# dependency "liblzma"
-# dependency "zlib"
+dependency "libxml2"
+dependency "libxslt"
+dependency "liblzma"
+dependency "zlib"
 dependency "libarchive"
 
 build do
