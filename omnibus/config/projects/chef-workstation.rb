fa9b4f7166475e0651ddff493621df81ebe0144a
Ashique P S <Ashique.saidalavi@progress.com>
2022-10-26 19:36:19 +0530

    Fixed the rubocop issue
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- omnibus/config/projects/chef-workstation.rb
+++ omnibus/config/projects/chef-workstation.rb
@@ -91,7 +91,6 @@ dependency "ruby-env-script"
 dependency "ruby-cleanup"
 dependency "workstation-gui"
 
-
 # further gem cleanup other projects might not yet want to use
 dependency "more-ruby-cleanup"
 
