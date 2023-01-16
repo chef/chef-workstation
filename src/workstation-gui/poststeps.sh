e96831182b05098798162e0eff8c346e32a42706
Vikram Karve <vikram.karve@progress.com>
2023-01-16 17:06:00 +0530

    use /Users/vkarve in paths
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -10,20 +10,20 @@ bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
 echo $?
 echo "Exit-code of secrets.sh script run"
 
-if [ ! -d "~/Library/LaunchAgents" ]; then
+if [ ! -d "$HOME/Library/LaunchAgents" ]; then
   echo $HOME
   echo "Creating LaunchAgents folder"
-  mkdir -p "~/Library/LaunchAgents"
+  mkdir -p "$HOME/Library/LaunchAgents"
   echo $?
   echo "Exit-code of LaunchAgents folder creation"
 fi
 
 echo "Copying plist to LaunchAgents"
-cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
+cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist $HOME/Library/LaunchAgents/.
 echo $?
 echo "Exit-code of plist copy"
 
 # Unload first, this will help reload the service on further upgrades
-sudo -u $USER launchctl unload ~/Library/LaunchAgents/io.chef.chef-workstation.plist
+sudo -u $USER launchctl unload $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
 
-sudo -u $USER launchctl load ~/Library/LaunchAgents/io.chef.chef-workstation.plist
+sudo -u $USER launchctl load $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
