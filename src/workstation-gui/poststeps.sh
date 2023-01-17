5f190297736ee095a6b4bb0059621911734492bd
Vikram Karve <vikram.karve@progress.com>
2023-01-17 12:52:39 +0530

    update error logs
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -6,24 +6,18 @@ chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp
 chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/log
 
 echo "Generating seed secrets"
-bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
-echo $?
-echo "Exit-code of secrets.sh script run"
+bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh || echo "Execution of secrets.sh failed"
 
 if [ ! -d "$HOME/Library/LaunchAgents" ]; then
-  echo $HOME
   echo "Creating LaunchAgents folder"
-  mkdir -p "$HOME/Library/LaunchAgents"
-  echo $?
-  echo "Exit-code of LaunchAgents folder creation"
+  mkdir -p "$HOME/Library/LaunchAgents" || echo "LaunchAgents folder creation failed"
 fi
 
 echo "Copying plist to LaunchAgents"
-cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist $HOME/Library/LaunchAgents/.
-echo $?
-echo "Exit-code of plist copy"
+cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist $HOME/Library/LaunchAgents/. || echo "plist copy to LaunchAgents failed"
 
 # Unload first, this will help reload the service on further upgrades
 sudo -u $USER launchctl unload $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
 
+echo "Loading agent to start puma service"
 sudo -u $USER launchctl load $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
