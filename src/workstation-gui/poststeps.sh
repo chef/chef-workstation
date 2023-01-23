7c5b6000f4515cd0e84e4f3baf1aad975e726b57
Vikram Karve <85881329+vkarve-chef@users.noreply.github.com>
2023-01-23 11:19:18 +0530

    INFWS-175 Tweaks in post-install script (#3028)
    * copy script out of app instead of move, maintain app integrity
    * create LaunchAgents folder if it doesn't exist
    * Refer to $HOME instead of ~ which isn't resolved
    * add few error logs in post install steps script
    
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- src/workstation-gui/poststeps.sh
+++ src/workstation-gui/poststeps.sh
@@ -5,11 +5,19 @@ chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/tmp
 
 chmod -R 777 /opt/chef-workstation/embedded/service/workstation-gui/log
 
-bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh
+echo "Generating seed secrets"
+bash /opt/chef-workstation/embedded/service/workstation-gui/secrets.sh || echo "Execution of secrets.sh failed"
 
-cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist ~/Library/LaunchAgents/
+if [ ! -d "$HOME/Library/LaunchAgents" ]; then
+  echo "Creating LaunchAgents folder"
+  mkdir -p "$HOME/Library/LaunchAgents" || echo "LaunchAgents folder creation failed"
+fi
+
+echo "Copying plist to LaunchAgents"
+cp /opt/chef-workstation/embedded/service/workstation-gui/config/io.chef.chef-workstation.plist $HOME/Library/LaunchAgents/. || echo "plist copy to LaunchAgents failed"
 
 # Unload first, this will help reload the service on further upgrades
-sudo -u $USER launchctl unload ~/Library/LaunchAgents/io.chef.chef-workstation.plist
+sudo -u $USER launchctl unload $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
 
-sudo -u $USER launchctl load ~/Library/LaunchAgents/io.chef.chef-workstation.plist
+echo "Loading agent to start puma service"
+sudo -u $USER launchctl load $HOME/Library/LaunchAgents/io.chef.chef-workstation.plist
