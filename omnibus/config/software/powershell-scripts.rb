fbcb8c17c3cd0180e322eead1619f64e7ba4e8d8
nikhil2611 <nikhilgupta2102@gmail.com>
2023-02-21 14:57:41 +0530

    removed the logs
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/powershell-scripts.rb
+++ omnibus/config/software/powershell-scripts.rb
@@ -26,40 +26,22 @@ build do
   block "Install windows powershell scripts" do
     # Copy the chef gem's distro stuff over
     chef_gem_path = File.expand_path("../..", shellout!("#{install_dir}/embedded/bin/gem which chef").stdout.chomp)
-    puts "-------chef_gem_path is ------#{chef_gem_path}"
 
     require "erb"
 
     template_file =  File.join("#{chef_gem_path}", "distro", "templates", "powershell", "chef", "chef.psm1.erb")
-    puts "-------template_file is ------#{template_file}"
-
     psm1_path = File.join("#{chef_gem_path}", "distro", "powershell", "chef")
-    puts "-------psm1_path is ------#{psm1_path}"
 
     create_directory(psm1_path)
-    puts "-------psm1_path is ------#{psm1_path}"
 
     chef_module_dir = "#{install_dir}/modules/chef"
-    puts "-------chef_module_dir is ------#{chef_module_dir}"
 
     create_directory(chef_module_dir)
-    puts "-------chef_module_dir is ------#{chef_module_dir}"
-
     template = ERB.new(IO.read(template_file))
-    puts "-------template is ------#{template}"
-
-
     chef_psm1 = template.result
-    puts "-------chef_psm1 is ------#{chef_psm1}"
-
     File.open(::File.join(psm1_path, "chef.psm1"), "w") { |f| f.write(chef_psm1) }
-    puts "-------chef.pam1 path is ------#{system "ls -al #{psm1_path}/"}"
-
     Dir.glob("#{chef_gem_path}/distro/powershell/chef/*").each do |file|
-    puts "-------file is ------#{file}"
-    puts "-------path is ------#{system "ls -al #{chef_gem_path}/distro/"}"
       copy_file(file, chef_module_dir)
-      puts "-------after-copy is ------#{system "ls -al #{chef_module_dir}"}"
     end
   end
 end
