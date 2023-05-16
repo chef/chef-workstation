7f54b8a987709b1def5772642213e7fb3cf65ab4
Vikram Karve <85881329+vkarve-chef@users.noreply.github.com>
2023-05-16 13:40:32 +0530

    restore chefspec ref
    Signed-off-by: Vikram Karve <vikram.karve@progress.com>

--- omnibus/verification/verify.rb
+++ omnibus/verification/verify.rb
@@ -201,8 +201,8 @@ module ChefWorkstation
         c.smoke_test { sh("#{bin("chef-run")} -v", env: { "CHEF_TELEMETRY_OPT_OUT" => "true" }) }
       end
 
-      add_component "chefspec-chef" do |c|
-        c.gem_base_dir = "chefspec-chef"
+      add_component "chefspec" do |c|
+        c.gem_base_dir = "chefspec"
         c.unit_test do
           bundle_install_mutex.synchronize { sh("#{embedded_bin("bundle")} install") }
           sh("#{embedded_bin("bundle")} exec #{embedded_bin("rake")} unit")
