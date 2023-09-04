9c03d0d797d242a29970bf823111e8309814e4ed
nikhil2611 <nikhilgupta2102@gmail.com>
2023-09-04 14:23:15 +0530

    added post-install steps in gems.rb
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/gems.rb
+++ omnibus/config/software/gems.rb
@@ -74,35 +74,41 @@ build do
   bundle "config set --local without '#{excluded_groups.join(" ")}'", env: env
   bundle "install --jobs 10", env: env
 
-  block do
-    gem_home = Gem.paths.home
+  puts "Current working directory is: ",Dir.pwd();
+  puts "--------gem_home is -----#{Gem.paths.home}-------"
 
-    puts "fixing bundle installed gems in #{gem_home}"
 
-    # Install gems from git repos.  This makes the assumption that there is a <gem_name>.gemspec and
-    # you can simply gem build + gem install the resulting gem, so nothing fancy.  This does not use
-    # rake install since we need --conservative --minimal-deps in order to not install duplicate gems.
-    #
-    Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
-      matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
-      next unless matches
+  ruby "post-bundle-install.rb", env: env
 
-      gem_name = File.basename(Dir["#{gempath}/*.gemspec"].first, ".gemspec")
-      # FIXME: should strip any valid ruby platform off of the gem_name if it matches
+  # block do
+  #   gem_home = Gem.paths.home
 
-      next unless gem_name
+  #   puts "fixing bundle installed gems in #{gem_home}"
 
-      # FIXME: should omit the gem which is in the current directory and not hard code chef
-      # next if %w{chef chef-universal-mingw-ucrt proxifier}.include?(gem_name)
+  #   # Install gems from git repos.  This makes the assumption that there is a <gem_name>.gemspec and
+  #   # you can simply gem build + gem install the resulting gem, so nothing fancy.  This does not use
+  #   # rake install since we need --conservative --minimal-deps in order to not install duplicate gems.
+  #   #
+  #   Dir["#{gem_home}/bundler/gems/*"].each do |gempath|
+  #     matches = File.basename(gempath).match(/.*-[A-Fa-f0-9]{12}/)
+  #     next unless matches
 
-      puts "re-installing #{gem_name}..."
+  #     gem_name = File.basename(Dir["#{gempath}/*.gemspec"].first, ".gemspec")
+  #     # FIXME: should strip any valid ruby platform off of the gem_name if it matches
 
-      Dir.chdir(gempath) do
-        system("gem build #{gem_name}.gemspec") or raise "gem build failed"
-        system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
-      end
-    end
-  end
+  #     next unless gem_name
+
+  #     # FIXME: should omit the gem which is in the current directory and not hard code chef
+  #     # next if %w{chef chef-universal-mingw-ucrt proxifier}.include?(gem_name)
+
+  #     puts "re-installing #{gem_name}..."
+
+  #     Dir.chdir(gempath) do
+  #       system("gem build #{gem_name}.gemspec") or raise "gem build failed"
+  #       system("gem install #{gem_name}*.gem --conservative --minimal-deps --no-document") or raise "gem install failed"
+  #     end
+  #   end
+  # end
 
   appbundle "knife", lockdir: project_dir, gem: "knife", without: %w{development}, env: env
   appbundle "foodcritic", lockdir: project_dir, gem: "chef_deprecations", without: %w{development test}, env: env
