062e8d68303616b05ea32b7e009d5d61de5a4a3c
Ashique P S <Ashique.saidalavi@progress.com>
2023-10-17 10:37:47 +0530

    Updated the version to avoid the CVEs
    Signed-off-by: Ashique P S <Ashique.saidalavi@progress.com>

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -6,7 +6,7 @@
 override "chef-analyze", version: "0.1.186"
 # /DO NOT MODIFY
 
-override "libarchive", version: "3.5.2"
+override "libarchive", version: "3.6.2"
 override "libffi", version: "3.4.2"
 override "libiconv", version: "1.16"
 override "liblzma", version: "5.2.5"
@@ -14,15 +14,15 @@ override "liblzma", version: "5.2.5"
 # libxslt 1.1.35 does not build successfully with libxml2 2.9.13 on Windows so we will pin
 # windows builds to libxslt 1.1.34 and libxml2 2.9.10 for now and followup later with the
 # work to fix that issue in IPACK-145.
-override "libxml2", version: windows? ? "2.9.10" : "2.9.13"
+override "libxml2", version: windows? ? "2.9.10" : "2.10.3"
 override "libxslt", version: windows? ? "1.1.34" : "1.1.35"
-
+override "go", version: "1.19.12"
 override "libyaml", version: "0.1.7"
 override "makedepend", version: "1.0.5"
-override "ncurses", version: "6.3"
-override :openssl, version: "1.1.1t" if mac_os_x?
+override "ncurses", version: "6.4"
+override :openssl, version: "1.1.1w"
 override "pkg-config-lite", version: "0.28-1"
-override "ruby", version: "3.1.2"
+override "ruby", version: "3.1.3"
 override "ruby-windows-devkit-bash", version: "3.1.23-4-msys-1.0.18"
 override "ruby-msys2-devkit", version: "3.1.2-1"
 override "rust", version: "1.37.0"
