2cdcc04eb06556915ddf84db8e15007158ba3844
Ashique Saidalavi <Ashique.saidalavi@progress.com>
2024-07-22 17:10:34 +0530

    Install knife on gems.rb file
    Signed-off-by: Ashique Saidalavi <Ashique.saidalavi@progress.com>

--- omnibus/config/software/gems.rb
+++ omnibus/config/software/gems.rb
@@ -74,6 +74,10 @@ build do
   bundle "config set --local without '#{excluded_groups.join(" ")}'", env: env
   bundle "install --jobs 10", env: env
 
+  command "gem list"
+
+  command "/opt/chef-workstation/embedded/bin/gem install knife --git https://github.com/chef/chef.git --branch ashiqueps/test-workstation-licensing-changes --glob knife/knife.gemspec --install-dir /opt/chef-workstation/embedded/lib/ruby/gems/3.1.0"
+
   ruby "post-bundle-install.rb", env: env
 
   command "gem list"
