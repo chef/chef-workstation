3090ddd2c3d5fb1a2e788cf36eb7e718296a18cb
nikhil2611 <nikhilgupta2102@gmail.com>
2025-03-05 13:19:59 +0530

    updated the curl to version 8.12.1 and openSSL to version 3.4.1
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/openssl.rb
+++ omnibus/config/software/openssl.rb
@@ -23,7 +23,7 @@ skip_transitive_dependency_licensing true
 dependency "cacerts"
 dependency "openssl-fips" if fips_mode?
 
-default_version "3.0.15"
+default_version "3.4.1"
 
 # Openssl builds engines as libraries into a special directory. We need to include
 # that directory in lib_dirs so omnibus can sign them during macOS deep signing.
@@ -35,7 +35,7 @@ source url: "https://www.openssl.org/source/openssl-#{version}.tar.gz", extract:
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz", extract: :lax_tar,
                 authorization: "X-JFrog-Art-Api:#{ENV["ARTIFACTORY_TOKEN"]}"
 
-version("3.0.15") { source sha256: "23c666d0edf20f14249b3d8f0368acaee9ab585b09e1de82107c66e1f3ec9533" }
+version("3.4.1") { source sha256: "002a2d6b30b58bf4bea46c43bdd96365aaf8daa6c428782aa4feee06da197df3" }
 
 relative_path "openssl-#{version}"
 
@@ -91,12 +91,12 @@ build do
     end
 
   # Patches
-  patch source: "openssl-3.0.1-do-not-install-docs.patch", env: env
+  patch source: "openssl-3.4.1-do-not-install-docs.patch", env: env
   # Some of the algorithms which are being used are deprecated in OpenSSL3 and moved to legacy provider.
   # We need those algorithms for the working of chef-workstation and other packages.
   # This patch will enable the legacy providers!
   configure_args << "enable-legacy"
-  patch source: "openssl-3.0.0-enable-legacy-provider.patch", env: env
+  patch source: "openssl-3.4.1-enable-legacy-provider.patch", env: env
 
   # Out of abundance of caution, we put the feature flags first and then
   # the crazy platform specific compiler flags at the end.
