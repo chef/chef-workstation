8f56bf4872b76bf2364d369b8b4c7d7dced7b5e4
Chef Expeditor <chef-ci@chef.io>
2025-04-15 20:30:45 +0000

    Bump chef-analyze CLI to 0.1.187
    This pull request was triggered automatically via Expeditor when chef-analyze 0.1.187 was merged to main.
    
    This change falls under the obvious fix policy so no Developer Certificate of Origin (DCO) sign-off is required.

--- omnibus_overrides.rb
+++ omnibus_overrides.rb
@@ -3,7 +3,7 @@
 # changes are merged to main Expeditor takes that version, runs a script to
 # replace it here and pushes a new commit / build through.
 
-override "chef-analyze", version: "0.1.186"
+override "chef-analyze", version: "0.1.187"
 # /DO NOT MODIFY
 
 override "libarchive", version: "3.7.5"
