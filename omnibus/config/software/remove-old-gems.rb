d9c444f9f3649419a2b0ee530a015eb86425a407
nikhil2611 <nikhilgupta2102@gmail.com>
2025-05-22 11:01:13 +0530

    updated var name to gembin
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -5,14 +5,14 @@ skip_transitive_dependency_licensing true
 
 build do
   block "Removing old versions of rexml < 3.3.6" do
-    gemfile = "#{install_dir}/embedded/bin/gem"
+    gembin = "#{install_dir}/embedded/bin/gem"
 
-    next unless File.exist?(gemfile)
+    next unless File.exist?(gembin)
 
     puts "Removing old versions of rexml < 3.3.6"
     env = with_standard_compiler_flags(with_embedded_path)
 
     # remove [-a] all rexml < 3.3.6 including [-x] executables and [-I] ignore dependencies
-    command "#{gemfile} uninstall rexml -v \"<3.3.6\" -a -x -I", env: env
+    command "#{gembin} uninstall rexml -v \"<3.3.6\" -a -x -I", env: env
   end
 end
