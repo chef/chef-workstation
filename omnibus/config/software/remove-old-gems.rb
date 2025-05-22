1497ab75c661a50cf4f0e29d295f2087910d6e51
Nikhil Gupta <35272911+nikhil2611@users.noreply.github.com>
2025-05-22 05:54:07 +0000

    Remove rexml versions with potential CVEs  (#3342)
    * removing rexml older version < 3.3.6
    
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>
    
    * added new line
    
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>
    
    * updated var name to gembin
    
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>
    
    ---------
    
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

---
+++ omnibus/config/software/remove-old-gems.rb
@@ -0,0 +1,18 @@
+name "remove-old-gems"
+
+license :project_license
+skip_transitive_dependency_licensing true
+
+build do
+  block "Removing old versions of rexml < 3.3.6" do
+    gembin = "#{install_dir}/embedded/bin/gem"
+
+    next unless File.exist?(gembin)
+
+    puts "Removing old versions of rexml < 3.3.6"
+    env = with_standard_compiler_flags(with_embedded_path)
+
+    # remove [-a] all rexml < 3.3.6 including [-x] executables and [-I] ignore dependencies
+    command "#{gembin} uninstall rexml -v \"<3.3.6\" -a -x -I", env: env
+  end
+end
