b5c63df32704bc3bcab588de42c02b32fc4b7ce9
poorndm <poorndm@progress.com>
2025-09-02 11:44:26 +0530

    fix syntax
    Signed-off-by: poorndm <poorndm@progress.com>

--- omnibus/omnibus.rb
+++ omnibus/omnibus.rb
@@ -33,9 +33,9 @@ use_git_caching false
 use_s3_caching true
 s3_access_key  ENV["AWS_ACCESS_KEY_ID"]
 s3_secret_key  ENV["AWS_SECRET_ACCESS_KEY"]
-s3_bucket      'opscode-omnibus-cache-private'
-s3_acl         'private'
-s3_region      'us-west-2'
+s3_bucket      "opscode-omnibus-cache-private"
+s3_acl         "private"
+s3_region      "us-west-2"
 
 build_retries 0
 fetcher_retries 3
