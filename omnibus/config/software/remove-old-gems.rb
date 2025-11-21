9529ae806a5c7a535b7f4dfe286287d3ec4b383c
nikhil2611 <nikhilgupta2102@gmail.com>
2025-11-21 17:19:10 +0530

    fixing resolv and imap CVEs
    Signed-off-by: nikhil2611 <nikhilgupta2102@gmail.com>

--- omnibus/config/software/remove-old-gems.rb
+++ omnibus/config/software/remove-old-gems.rb
@@ -16,4 +16,32 @@ build do
     # Note: We do NOT use -x flag to avoid removing executables/binstubs
     command "#{gembin} uninstall rexml -v \"<3.4.2\" -a -I", env: env
   end
+
+  block "Removing old versions of resolv < 0.2.3" do
+    gembin = "#{install_dir}/embedded/bin/gem"
+
+    next unless File.exist?(gembin)
+
+    puts "Removing old versions of resolv < 0.2.3"
+    env = with_standard_compiler_flags(with_embedded_path)
+
+    # remove [-a] all resolv < 0.2.3 and [-I] ignore dependencies
+    # CVE: GHSA-xh69-987w-hrp8
+    # Note: We do NOT use -x flag to avoid removing executables/binstubs
+    command "#{gembin} uninstall resolv -v \"<0.2.3\" -a -I", env: env
+  end
+
+  block "Removing old versions of net-imap < 0.2.5" do
+    gembin = "#{install_dir}/embedded/bin/gem"
+
+    next unless File.exist?(gembin)
+
+    puts "Removing old versions of net-imap < 0.2.5"
+    env = with_standard_compiler_flags(with_embedded_path)
+
+    # remove [-a] all net-imap < 0.2.5 and [-I] ignore dependencies
+    # CVE: GHSA-j3g3-5qv5-52mj
+    # Note: We do NOT use -x flag to avoid removing executables/binstubs
+    command "#{gembin} uninstall net-imap -v \"<0.2.5\" -a -I", env: env
+  end
 end
