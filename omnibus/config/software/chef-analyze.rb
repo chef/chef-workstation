96ca0443ad4a4362173b8dd339deb842edd6ec37
nikhil2611 <ngupta@progress.com>
2026-02-23 17:33:15 +0530

    updated chef-analyze to 0.1.190 and go in main-chef-wrapper to 1.25.6
    Signed-off-by: nikhil2611 <ngupta@progress.com>

--- omnibus/config/software/chef-analyze.rb
+++ omnibus/config/software/chef-analyze.rb
@@ -1,11 +1,11 @@
 name "chef-analyze"
-default_version "0.1.187"  # updated to latest version
+default_version "0.1.190"  # updated to latest version
 license "Apache-2.0"
 license_file "LICENSE"
 
 # versions_list: https://github.com/chef/chef-analyze/tags filter=*.tar.gz
 source url: "https://github.com/chef/chef-analyze/archive/refs/tags/#{default_version}.tar.gz",
-       sha256: "b0a97ee948c312ec97c0acf4ab8f29b5806afc91292279f2bcd10504e890a7cb"
+       sha256: "eb06e4adf786affcdaad0cd36ebac4d5cad5783fefdc60ea9591b71a7ea38631"
 
 # Update the internal_source URL to match the source URL
 internal_source url: "#{ENV["ARTIFACTORY_REPO_URL"]}/#{name}/#{name}-#{version}.tar.gz",
